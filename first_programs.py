6af249209ff2a6127da442fcfa644253aa8f080e
Deepak gupta <deepakgupta.bca22@poddarinstitute.org>
2024-11-04 20:38:10 +0530

    first_programs.py

---
+++ first_programs.py
@@ -0,0 +1,16 @@
+print("Hello Everyone ")
+
+
+a = 5
+b = 10
+print(a+b)              # 15
+
+
+c = a+b 
+print(c)        # 15
+
+a = "Hello world "                                     # print string using variable 
+print(a)
+
+
+
